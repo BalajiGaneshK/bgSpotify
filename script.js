f775553a01eb68a1f57d49b3aefbdfeba1ff860e
BalajiGaneshK <kbalajiganeshdav@gmail.com>
2021-02-17 00:05:35 +0530

    trying auth

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let response_type = "code";
 let client_id = "465a082539fb4ba7a216fb3bec7f1ffc";
-let redirect_uri = "";
+let redirect_uri = "https://bgspotify.netlify.app/index2.html";
+let code_challenge_method = "S256";
+let code_challenge = "a43a4462962f4975353cdac1b7e5d1a32fa7e523ac69855fbd782146807da45c";  //code verifier: bg-spotify
+
+
+let authorization_uri = "https://accounts.spotify.com/authorize?response_type=" + response_type +
+    "& client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&code_challenge=" + code_challenge + "&code_challenge_method=" + code_challenge_method;
+
+    
+// state (CORS ) and scope( specific api end points) not included
